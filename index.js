aea39f9832e1f5ecf04ec2f7ab98635594f8af46
Jothsa <58094796+Jothsa@users.noreply.github.com>
2023-05-13 14:22:32 -0500

    logical properties

--- index.js
+++ index.js
@@ -1,96 +1,111 @@
-function trbl (prefix) {
-  const rules = []
+function trbl(prefix) {
+  const rules = [];
 
   if (prefix) {
-    rules.push(prefix)
-    prefix = prefix + '-'
+    rules.push(prefix);
+    prefix = prefix + "-";
   } else {
-    prefix = ''
+    prefix = "";
   }
 
   return rules.concat([
-    prefix + 'top',
-    prefix + 'right',
-    prefix + 'bottom',
-    prefix + 'left'
-  ])
+    prefix + "top",
+    prefix + "right",
+    prefix + "bottom",
+    prefix + "left",
+  ]);
 }
 
-function minMax (suffix) {
-  return [suffix, 'min-' + suffix, 'max-' + suffix]
+function startEnd(prefix) {
+  return [prefix + "-start", prefix + "-end"];
 }
 
-function border (infix) {
+function minMax(suffix) {
+  return [suffix, "min-" + suffix, "max-" + suffix];
+}
+
+function border(infix) {
   if (infix) {
-    infix = '-' + infix
+    infix = "-" + infix;
   } else {
-    infix = ''
+    infix = "";
   }
 
   return [
-    'border' + infix,
-    'border' + infix + '-width',
-    'border' + infix + '-style',
-    'border' + infix + '-color',
-    'border' + infix + '-radius'
-  ]
+    "border" + infix,
+    "border" + infix + "-width",
+    "border" + infix + "-style",
+    "border" + infix + "-color",
+    "border" + infix + "-radius",
+  ];
 }
 
-const cssModules = []
-  .concat([
-    'composes'
-  ])
+const cssModules = [].concat(["composes"]);
 
-const reset = ['all']
+const reset = ["all"];
 
 const positioning = []
-  .concat([
-    'position',
-    'z-index'
-  ])
+  .concat(["position", "z-index"])
   .concat(trbl())
+  .concat(startEnd("inset-block"))
+  .concat(startEnd("inset-inline"));
 
 const displayAndBoxModel = []
+  .concat(["display", "overflow"])
+  .concat(minMax("width"))
+  .concat(minMax("height"))
+  .concat(minMax("inline-size"))
+  .concat(minMax("block-size"))
   .concat([
-    'display',
-    'overflow'
+    "box-sizing",
+    "flex",
+    "flex-basis",
+    "flex-direction",
+    "flex-flow",
+    "flex-grow",
+    "flex-shrink",
+    "flex-wrap",
+    "align-content",
+    "align-items",
+    "align-self",
+    "justify-content",
+    "order",
   ])
-  .concat(minMax('width'))
-  .concat(minMax('height'))
-  .concat([
-    'box-sizing',
-    'flex',
-    'flex-basis',
-    'flex-direction',
-    'flex-flow',
-    'flex-grow',
-    'flex-shrink',
-    'flex-wrap',
-    'align-content',
-    'align-items',
-    'align-self',
-    'justify-content',
-    'order'
-  ])
-  .concat(trbl('padding'))
-  .concat([]
-    .concat(border())
-    .concat(border('top'))
-    .concat(border('right'))
-    .concat(border('bottom'))
-    .concat(border('left')))
-  .concat(trbl('margin'))
+  .concat(trbl("padding"))
+  .concat(startEnd("padding-block"))
+  .concat(startEnd("padding-inline"))
+  .concat(
+    []
+      .concat(border())
+      .concat(border("top"))
+      .concat(border("right"))
+      .concat(border("bottom"))
+      .concat(border("left"))
+      .concat(border("block-start"))
+      .concat(border("block-end"))
+      .concat(border("inline-start"))
+      .concat(border("inline-end"))
+      .concat([
+        "border-start-start-radius",
+        "border-start-end-radius",
+        "border-end-start-radius",
+        "border-end-end-radius",
+      ])
+  )
+  .concat(trbl("margin"))
+  .concat(startEnd("margin-block"))
+  .concat(startEnd("margin-inline"));
 
 module.exports = {
-  plugins: 'stylelint-order',
+  plugins: "stylelint-order",
   rules: {
-    'order/properties-order': [
+    "order/properties-order": [
       []
         .concat(cssModules)
         .concat(reset)
         .concat(positioning)
         .concat(displayAndBoxModel),
-      { unspecified: 'bottomAlphabetical' }
-    ]
-  }
-}
+      { unspecified: "bottomAlphabetical" },
+    ],
+  },
+};
--- test/test.js
+++ test/test.js
@@ -1,34 +1,68 @@
-const assert = require('assert')
-const fs = require('fs')
-const path = require('path')
-const stylelint = require('stylelint')
+const assert = require("assert");
+const fs = require("fs");
+const path = require("path");
+const stylelint = require("stylelint");
 
-function testConfigFile () {
+function testConfigFile() {
   assert.doesNotThrow(() => {
-    require(path.join(__dirname, '..', 'index.js'))
-  })
+    require(path.join(__dirname, "..", "index.js"));
+  });
 
-  return Promise.resolve()
+  return Promise.resolve();
 }
 
-function testOrder () {
-  const fixture = fs.readFileSync(path.join(__dirname, 'fixture.css'), 'utf8')
-  const expected = fs.readFileSync(path.join(__dirname, 'expected.css'), 'utf8')
+function testOrder() {
+  const fixture = fs.readFileSync(path.join(__dirname, "fixture.css"), "utf8");
+  const expected = fs.readFileSync(
+    path.join(__dirname, "expected.css"),
+    "utf8"
+  );
 
-  return stylelint.lint({
-    code: fixture,
-    config: require('..'),
-    fix: true
-  }).then(result => {
-    assert.strictEqual(result.errored, false)
-    assert.strictEqual(result.output, expected, 'Stylelint output does not equal expected output')
-  })
+  return stylelint
+    .lint({
+      code: fixture,
+      config: require(".."),
+      fix: true,
+    })
+    .then((result) => {
+      assert.strictEqual(result.errored, false);
+      assert.strictEqual(
+        result.output,
+        expected,
+        "Stylelint output does not equal expected output"
+      );
+    });
 }
 
-Promise
-  .all([testConfigFile(), testOrder()])
-  .then(() => console.log('OK'))
-  .catch(e => {
-    console.error(e.name, e.message)
-    process.exit(-1)
-  })
+function testLogicalPropertiesOrder() {
+  const fixture = fs.readFileSync(
+    path.join(__dirname, "fixtureLogicalProperties.css"),
+    "utf8"
+  );
+  const expected = fs.readFileSync(
+    path.join(__dirname, "expectedLogicalProperties.css"),
+    "utf8"
+  );
+
+  return stylelint
+    .lint({
+      code: fixture,
+      config: require(".."),
+      fix: true,
+    })
+    .then((result) => {
+      assert.strictEqual(result.errored, false);
+      assert.strictEqual(
+        result.output,
+        expected,
+        "Stylelint output does not equal expected output"
+      );
+    });
+}
+
+Promise.all([testConfigFile(), testOrder(), testLogicalPropertiesOrder()])
+  .then(() => console.log("OK"))
+  .catch((e) => {
+    console.error(e.name, e.message);
+    process.exit(-1);
+  });
