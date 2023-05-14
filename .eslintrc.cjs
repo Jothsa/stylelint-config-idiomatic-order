8075893fce38f926fb2742c11aff9aa5ce6ba855
Jothsa <58094796+Jothsa@users.noreply.github.com>
2023-05-13 22:33:40 -0500

    tooling

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    // TODO Add type checking rules
+    // 'plugin:@typescript-eslint/re'
+    'prettier',
+  ],
+  plugins: [ '@typescript-eslint'],
+  ignorePatterns: ['*.cjs'],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2020,
+  },
+  env: {
+    browser: true,
+    es2017: true,
+    node: true,
+  },
+};
---
+++ .prettierrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  tabWidth: 2,
+  singleQuote: true,
+  trailingComma: 'all',
+  semi: true,
+  bracketSameLine: true,
+  overrides: [{ files: '*.svelte', options: { parser: 'svelte' } }],
+};
